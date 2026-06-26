ba14bb0c28f44badab8168c582aece91e87d6c2f
LanreAkintayo <akintayolanre2019@gmail.com>
2026-06-26 09:06:52 +0100

    feat: made a little change

--- mutex_stuff/challenge_5.go
+++ mutex_stuff/challenge_5.go
@@ -21,9 +21,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"hash/fnv"
 	"sync"
-	"fmt"
 )
 
 type Driver struct {
@@ -74,11 +74,12 @@ func (m *MatchMaker) UpdateDriverLocation(location string, newLocation string, d
 			delete(oldShard.areaDrivers[location], driverId)
 			driver.location = newLocation
 
-			if oldShard.areaDrivers[newLocation] == nil {
-				oldShard.areaDrivers[newLocation] = make(map[string]*Driver)
+			innerMap := oldShard.areaDrivers[newLocation]
+			if innerMap == nil {
+				innerMap = make(map[string]*Driver)
+				oldShard.areaDrivers[newLocation] = innerMap
 			}
-
-			oldShard.areaDrivers[newLocation][driverId] = driver
+			innerMap[driverId] = driver
 
 		}
 		return
@@ -88,6 +89,7 @@ func (m *MatchMaker) UpdateDriverLocation(location string, newLocation string, d
 	driver, exist := oldShard.areaDrivers[location][driverId]
 
 	if exist {
+		driver.location = newLocation
 		delete(oldShard.areaDrivers[location], driverId)
 	}
 	oldShard.keyLock.Unlock()
@@ -95,7 +97,6 @@ func (m *MatchMaker) UpdateDriverLocation(location string, newLocation string, d
 	if exist {
 		newShard.keyLock.Lock()
 
-		driver.location = newLocation
 		if newShard.areaDrivers[newLocation] == nil {
 			newShard.areaDrivers[newLocation] = make(map[string]*Driver)
 		}
@@ -134,9 +135,9 @@ func (m *MatchMaker) RequestRide(fromLocation string) (*Driver, error) {
 func Challenge5() {
 	// Initialize a match maker with 5 distinct shards
 	noOfShards := 5
-	matchMaker := MatchMaker {
+	matchMaker := MatchMaker{
 		noOfShards: noOfShards,
-		shards: make([]*Shard, 5),
+		shards:     make([]*Shard, 5),
 	}
 
 	// Explicitly allocate memory for every single shard and its inner map
@@ -145,9 +146,9 @@ func Challenge5() {
 	}
 
 	// Let's seed an initial driver directly into the system state
-	newDriver := Driver {
-		driverId: "001",
-		location: "Ikeja",
+	newDriver := Driver{
+		driverId:    "001",
+		location:    "Ikeja",
 		isAvailable: true,
 	}
 	ikejaIndex := getShardIndex("Ikeja", noOfShards)
@@ -158,10 +159,10 @@ func Challenge5() {
 	fmt.Println("Driver 001 is available in Ikeja")
 
 	var goTrackers sync.WaitGroup
-	goTrackers.Add(2)	// Wait for 2 goroutines
+	goTrackers.Add(2) // Wait for 2 goroutines
 
 	// First one will request a ride in Ikeja
-	go func(){
+	go func() {
 		defer goTrackers.Done()
 		fmt.Println("Requesting a ride...")
 		driverPointer, err := matchMaker.RequestRide("Ikeja")
@@ -173,14 +174,13 @@ func Challenge5() {
 	}()
 
 	// Second one will update driver location from "Ikeja" to "Lagos"
-	go func(){
+	go func() {
 		defer goTrackers.Done()
 		fmt.Println("Changing location from Ikeja to Lekki")
 		matchMaker.UpdateDriverLocation("Ikeja", "Lekki", "001")
 		fmt.Println("Location updated successfully!")
 	}()
 
-	
 	goTrackers.Wait()
 
 	fmt.Println("Done!")
